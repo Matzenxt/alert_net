37b32451dd33e402bad699e421df11055112ddcf
Matthias Lodner <matthias@lodner.de>
2024-03-27 20:37:00 +0100

    Added audio.

--- mc_code/test_variable_safe/test_variable_safe.ino
+++ mc_code/test_variable_safe/test_variable_safe.ino
@@ -4,6 +4,20 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
+// Audio setup block
+#include "DFRobotDFPlayerMini.h"
+
+#if (defined(ARDUINO_AVR_UNO) || defined(ESP8266))   // Using a soft serial port
+  #include <SoftwareSerial.h>
+  SoftwareSerial softSerial(/*rx =*/D5, /*tx =*/D6);
+  #define FPSerial softSerial
+#else
+  #define FPSerial Serial1
+#endif
+
+DFRobotDFPlayerMini myDFPlayer;
+void printAudioDetail(uint8_t type, int value);
+
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
 #include <WebSocketsClient.h>
@@ -11,7 +25,7 @@
 // Wifi:
 WiFiManager wifi_manager;
 // select which pin will trigger the configuration portal when set to LOW
-#define TRIGGER_PIN D6 
+#define TRIGGER_PIN D1
 int timeout = 120; // seconds to run for
 
 // Websocket:
@@ -116,64 +130,89 @@ void configModeCallback(WiFiManager *myWiFiManager) {
   Serial.println(WiFi.softAPIP());
 }
 
-void wifi_config_init(bool forceConfig) {
-  WiFi.mode(WIFI_STA);
+void initAudio() {
+  #if (defined ESP32)
+    FPSerial.begin(9600, SERIAL_8N1, /*rx =*/D3, /*tx =*/D2);
+  #else
+    FPSerial.begin(9600);
+  #endif
 
-  wifi_manager.setSaveConfigCallback(saveConfigCallback);
-  wifi_manager.setAPCallback(configModeCallback);
-
-  // Custom elements:
-  WiFiManagerParameter custom_server_ip("server_ip", "Server IP", server_ip, 40);
-  char convertedPortValue[6];
-  sprintf(convertedPortValue, "%d", server_port);
-  WiFiManagerParameter custom_server_port("server_port", "Server Port", convertedPortValue, 7);
-  WiFiManagerParameter custom_area("area", "Bereich", area, 50);
-  WiFiManagerParameter custom_description("description", "Bezeichnung", description, 50);
-
-  wifi_manager.addParameter(&custom_server_ip);
-  wifi_manager.addParameter(&custom_server_port);
-  wifi_manager.addParameter(&custom_area);
-  wifi_manager.addParameter(&custom_description);
-
-  //wifi_manager.resetSettings();
-
-  if (forceConfig) {
-    if (!wifi_manager.startConfigPortal("AlertNet", "123456789")) {
-      Serial.println("Failed to connect and hit timeout");
-      delay(3000);
-      ESP.restart();
-      delay(5000);
-    }
-  } else {
-    if (!wifi_manager.autoConnect("AlertNet", "123456789")) {
-            Serial.println("Failed to connect and hit timeout");
-      delay(3000);
-      ESP.restart();
-      delay(5000);
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(FPSerial, /*isACK = */true, /*doReset = */false)) {  //Use serial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
     }
   }
+  Serial.println(F("DFPlayer Mini online."));
 
-  Serial.println("");
-  Serial.println("Wifi connected");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
+  myDFPlayer.play(1);  //Play the first mp3
+}
 
-  strncpy(server_ip, custom_server_ip.getValue(), sizeof(server_ip));
-  server_port = (uint16_t)atol(custom_server_port.getValue());
-  strncpy(area, custom_area.getValue(), sizeof(area));
-  strncpy(description, custom_description.getValue(), sizeof(description));
-
-  Serial.print("Server: ");
-  Serial.println(server_ip);
-  Serial.print("Port: ");
-  Serial.println(server_port);
-  Serial.print("Bereich: ");
-  Serial.println(area);
-  Serial.print("Description: ");
-  Serial.println(description);
-
-  if (shouldSaveConfig) {
-    saveConfigFile();
+void printAudioDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      Serial.println(F("Time Out!"));
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
   }
 }
 
@@ -341,6 +380,8 @@ void setup() {
   }
 
 
+  initAudio();
+
 	// server address, port and URL
   char uri [52];
   strcpy(uri, "/");
@@ -383,4 +424,31 @@ void loop() {
     webSocket.sendTXT("Reboot");
     counter = counter + 1;
   }
+
+  if(digitalRead(motionPin) == HIGH) {
+    if(pirState == LOW) {
+      digitalWrite(LED_BUILTIN, LOW);
+
+      Serial.println("Bewegung");
+      //myDFPlayer.play(1);
+
+      webSocket.sendTXT("Bewegung");
+
+      pirState = HIGH;
+
+    }
+  } else {
+    if(pirState == HIGH) {
+      digitalWrite(LED_BUILTIN, HIGH);
+
+      Serial.println("Nichts");
+      pirState = LOW;
+    }
+  }
+
+  if (myDFPlayer.available()) {
+    printAudioDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
 }
+
+
