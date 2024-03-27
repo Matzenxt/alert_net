d914cc72f0384cfb47024847979127d47d229623
Matthias Lodner <matthias@lodner.de>
2024-03-27 18:55:25 +0100

    Added wifi manager with custom config fields that are saved to file.

--- mc_code/test_variable_safe/test_variable_safe.ino
+++ mc_code/test_variable_safe/test_variable_safe.ino
@@ -1,13 +1,20 @@
+#include <FS.h>
+#include <SPI.h>
+
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
+#include <WiFiManager.h>
 #include <WebSocketsClient.h>
 
+// Wifi:
+WiFiManager wifi_manager;
+// select which pin will trigger the configuration portal when set to LOW
+#define TRIGGER_PIN D6 
+int timeout = 120; // seconds to run for
 
-// Wifi and websocket block:
-ESP8266WiFiMulti WiFiMulti;
+// Websocket:
 WebSocketsClient webSocket;
 
 unsigned long messageInterval = 5000;
@@ -24,6 +31,151 @@ bool reboot = false;
 int motionPin = D1;
 int pirState = LOW;
 
+// Config:
+#define JSON_CONFIG_FILE "/config.json"
+bool shouldSaveConfig = true;
+
+char server_ip[40] = "192.168.0.88";
+uint16_t server_port = 3000;
+char area[50] = "test";
+char description[50] = "Gerät 1";
+
+void saveConfigFile() {
+  Serial.println(F("Saving configuration..."));
+
+  JsonDocument config;
+  config["server_ip"] = server_ip;
+  config["server_port"] = server_port;
+  config["area"] = area;
+  config["description"] = description;
+
+  File configFile = SPIFFS.open(JSON_CONFIG_FILE, "w");
+  if (!configFile) {
+    Serial.println("Failed to open config file for writing.");
+  }
+
+  serializeJsonPretty(config, Serial);
+  if (serializeJsonPretty(config, configFile) == 0) {
+    Serial.println(F("Failed to write config to file"));
+  }
+
+  configFile.close();
+}
+
+bool loadConfigFile() {
+  //SPIFFS.format();
+
+  Serial.println("Mounting file system.");
+
+  if (SPIFFS.begin()) {
+    Serial.println("Mounted file system");
+
+    if (SPIFFS.exists(JSON_CONFIG_FILE)) {
+      Serial.println("Reading config file.");
+
+      File configFile = SPIFFS.open(JSON_CONFIG_FILE, "r");
+      if (configFile) {
+        Serial.println("Open config file.");
+        JsonDocument config;
+        DeserializationError error = deserializeJson(config, configFile);
+        serializeJsonPretty(config, Serial);
+
+        if (!error) {
+          Serial.println("Parsing config file");
+
+          strcpy(server_ip, config["server_ip"]);
+          server_port = config["server_port"].as<uint16_t>();
+          strcpy(area, config["area"]);
+          strcpy(description, config["description"]);
+
+          return true;
+        } else {
+          Serial.println("Failed to parse config file");
+        }
+      }
+    }
+  } else {
+    Serial.println("Failed to mount file system");
+  }
+
+  return false;
+}
+
+void saveConfigCallback() {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+void configModeCallback(WiFiManager *myWiFiManager) {
+  Serial.println("Entered configruation mode");
+
+  Serial.print(" Config SSID: ");
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+
+  Serial.print(" Config IP address: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void wifi_config_init(bool forceConfig) {
+  WiFi.mode(WIFI_STA);
+
+  wifi_manager.setSaveConfigCallback(saveConfigCallback);
+  wifi_manager.setAPCallback(configModeCallback);
+
+  // Custom elements:
+  WiFiManagerParameter custom_server_ip("server_ip", "Server IP", server_ip, 40);
+  char convertedPortValue[6];
+  sprintf(convertedPortValue, "%d", server_port);
+  WiFiManagerParameter custom_server_port("server_port", "Server Port", convertedPortValue, 7);
+  WiFiManagerParameter custom_area("area", "Bereich", area, 50);
+  WiFiManagerParameter custom_description("description", "Bezeichnung", description, 50);
+
+  wifi_manager.addParameter(&custom_server_ip);
+  wifi_manager.addParameter(&custom_server_port);
+  wifi_manager.addParameter(&custom_area);
+  wifi_manager.addParameter(&custom_description);
+
+  //wifi_manager.resetSettings();
+
+  if (forceConfig) {
+    if (!wifi_manager.startConfigPortal("AlertNet", "123456789")) {
+      Serial.println("Failed to connect and hit timeout");
+      delay(3000);
+      ESP.restart();
+      delay(5000);
+    }
+  } else {
+    if (!wifi_manager.autoConnect("AlertNet", "123456789")) {
+            Serial.println("Failed to connect and hit timeout");
+      delay(3000);
+      ESP.restart();
+      delay(5000);
+    }
+  }
+
+  Serial.println("");
+  Serial.println("Wifi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  strncpy(server_ip, custom_server_ip.getValue(), sizeof(server_ip));
+  server_port = (uint16_t)atol(custom_server_port.getValue());
+  strncpy(area, custom_area.getValue(), sizeof(area));
+  strncpy(description, custom_description.getValue(), sizeof(description));
+
+  Serial.print("Server: ");
+  Serial.println(server_ip);
+  Serial.print("Port: ");
+  Serial.println(server_port);
+  Serial.print("Bereich: ");
+  Serial.println(area);
+  Serial.print("Description: ");
+  Serial.println(description);
+
+  if (shouldSaveConfig) {
+    saveConfigFile();
+  }
+}
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 	switch(type) {
@@ -101,9 +253,10 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 }
 
 void setup() {
-  Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(TRIGGER_PIN, INPUT_PULLUP);
 
+  Serial.begin(115200);
 	Serial.setDebugOutput(true);
 
 	Serial.println();
@@ -116,17 +269,84 @@ void setup() {
 		delay(1000);
 	}
 
-	WiFiMulti.addAP("Privat-Wlan", "99842DA5D0");
-	WiFiMulti.addAP("Privat W-LAN", "99842DA5D0");
+  bool forceConfig = false;
 
-	//WiFi.disconnect();
-	while(WiFiMulti.run() != WL_CONNECTED) {
-		delay(100);
-	}
+  if (digitalRead(TRIGGER_PIN) == LOW) {
+    forceConfig = true;
+  }
+
+  bool spiffsSetup = loadConfigFile();
+  if (!spiffsSetup) {
+    Serial.println(F("Forcing config mode as there is no saved config!"));
+    forceConfig = true;
+  }
+
+  WiFi.mode(WIFI_STA);
+
+  wifi_manager.setSaveConfigCallback(saveConfigCallback);
+  wifi_manager.setAPCallback(configModeCallback);
+
+  // Custom elements:
+  WiFiManagerParameter custom_server_ip("server_ip", "Server IP", server_ip, 40);
+  char convertedPortValue[6];
+  sprintf(convertedPortValue, "%d", server_port);
+  WiFiManagerParameter custom_server_port("server_port", "Server Port", convertedPortValue, 7);
+  WiFiManagerParameter custom_area("area", "Bereich", area, 50);
+  WiFiManagerParameter custom_description("description", "Bezeichnung", description, 50);
+
+  wifi_manager.addParameter(&custom_server_ip);
+  wifi_manager.addParameter(&custom_server_port);
+  wifi_manager.addParameter(&custom_area);
+  wifi_manager.addParameter(&custom_description);
+
+  //wifi_manager.resetSettings();
+
+  if (forceConfig) {
+    if (!wifi_manager.startConfigPortal("AlertNet", "123456789")) {
+      Serial.println("Failed to connect and hit timeout");
+      delay(3000);
+      ESP.restart();
+      delay(5000);
+    }
+  } else {
+    if (!wifi_manager.autoConnect("AlertNet", "123456789")) {
+            Serial.println("Failed to connect and hit timeout");
+      delay(3000);
+      ESP.restart();
+      delay(5000);
+    }
+  }
+
+  Serial.println("");
+  Serial.println("Wifi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  strncpy(server_ip, custom_server_ip.getValue(), sizeof(server_ip));
+  server_port = (uint16_t)atol(custom_server_port.getValue());
+  strncpy(area, custom_area.getValue(), sizeof(area));
+  strncpy(description, custom_description.getValue(), sizeof(description));
+
+  Serial.print("Server: ");
+  Serial.println(server_ip);
+  Serial.print("Port: ");
+  Serial.println(server_port);
+  Serial.print("Bereich: ");
+  Serial.println(area);
+  Serial.print("Description: ");
+  Serial.println(description);
+
+  if (shouldSaveConfig) {
+    saveConfigFile();
+  }
+
 
 	// server address, port and URL
-	//webSocket.begin("192.168.123.160", 3000, "/laden");
-	webSocket.begin("192.168.0.88", 3000, "/kino");
+  char uri [52];
+  strcpy(uri, "/");
+  strcpy(uri, area);
+
+	webSocket.begin(server_ip, server_port, uri);
 
 	// event handler
 	webSocket.onEvent(webSocketEvent);
@@ -148,6 +368,7 @@ void setup() {
 int counter = 0;
 
 void loop() {
+  // Websocket part:
 	webSocket.loop();
   
   if (connected && lastUpdate + messageInterval < millis()) {
@@ -163,5 +384,3 @@ void loop() {
     counter = counter + 1;
   }
 }
-
-
